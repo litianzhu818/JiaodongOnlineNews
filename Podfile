bd36fb7a66b135da2cf32dfc2a9cfa63cc9ff46c
zhang yi <intotherainzy@gmail.com>
2013-05-19 14:48:17 +0800

    调整目录结构，项目分为Classes,Resources,Others三个根目录，国际化目录作为子目录。引入Nimbus,JSONKit类库。

--- Podfile
+++ Podfile
@@ -1,3 +1,6 @@
 platform:ios
 
 pod 'AFNetworking','0.10.1'
+pod 'Nimbus/NetworkControllers','0.9.3'
+pod 'Nimbus/NetworkImage','0.9.3'
+pod 'JSONKit'
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -12,3 +12,28 @@
 #define COCOAPODS_VERSION_MINOR_AFNetworking 10
 #define COCOAPODS_VERSION_PATCH_AFNetworking 1
 
+// JSONKit
+#define COCOAPODS_POD_AVAILABLE_JSONKit
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 1.5pre.
+
+// Nimbus/Core
+#define COCOAPODS_POD_AVAILABLE_Nimbus_Core
+#define COCOAPODS_VERSION_MAJOR_Nimbus_Core 0
+#define COCOAPODS_VERSION_MINOR_Nimbus_Core 9
+#define COCOAPODS_VERSION_PATCH_Nimbus_Core 3
+
+// Nimbus/NetworkControllers
+#define COCOAPODS_POD_AVAILABLE_Nimbus_NetworkControllers
+#define COCOAPODS_VERSION_MAJOR_Nimbus_NetworkControllers 0
+#define COCOAPODS_VERSION_MINOR_Nimbus_NetworkControllers 9
+#define COCOAPODS_VERSION_PATCH_Nimbus_NetworkControllers 3
+
+// Nimbus/NetworkImage
+#define COCOAPODS_POD_AVAILABLE_Nimbus_NetworkImage
+#define COCOAPODS_VERSION_MAJOR_Nimbus_NetworkImage 0
+#define COCOAPODS_VERSION_MINOR_Nimbus_NetworkImage 9
+#define COCOAPODS_VERSION_PATCH_Nimbus_NetworkImage 3
+
