9da2fb3c208dafb49d598739ecbffc4aabc1f853
陈鹏 <chenpeng@jiaodongdeMacBook-Pro.local>
2013-05-30 10:05:19 +0800

    添加“关于我们”界面

--- JiaodongOnlineNews/Classes/JDORightViewController.h
+++ JiaodongOnlineNews/Classes/JDORightViewController.h
@@ -6,6 +6,7 @@
 @interface JDORightViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
 
 @property (nonatomic, retain) IBOutlet UITableView* tableView;
+- (IBAction)onAboutClick:(id)sender;
 
 - (IBAction)onSettingClick:(id)sender;
 
