e7a60e038c8f97dd868989903afd02065f2ced08
Roc <cuipengxiang@gmail.com>
2013-06-06 16:11:51 +0800

    pull

---
+++ JiaodongOnlineNews/Classes/JDOConvenienceViewController.m
@@ -0,0 +1,38 @@
+//
+//  JDOConvenienceViewController.m
+//  JiaodongOnlineNews
+//
+//  Created by Roc on 13-6-6.
+//  Copyright (c) 2013年 胶东在线. All rights reserved.
+//
+
+#import "JDOConvenienceViewController.h"
+
+@interface JDOConvenienceViewController ()
+
+@end
+
+@implementation JDOConvenienceViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
