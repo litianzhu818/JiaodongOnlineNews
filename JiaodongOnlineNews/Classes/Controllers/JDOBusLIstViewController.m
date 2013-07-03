8cc67fa8e2bb0628f4644832121cb67fb0d15ea1
Roc <cuipengxiang@gmail.com>
2013-07-03 10:04:05 +0800

    添加公交列表界面

---
+++ JiaodongOnlineNews/Classes/Controllers/JDOBusLIstViewController.m
@@ -0,0 +1,102 @@
+//
+//  JDOBusLIstViewController.m
+//  JiaodongOnlineNews
+//
+//  Created by Roc on 13-7-2.
+//  Copyright (c) 2013年 胶东在线. All rights reserved.
+//
+
+#import "JDOBusLIstViewController.h"
+#import "JDOHttpClient.h"
+#import "JDONewsModel.h"
+
+@interface JDOBusLIstViewController ()
+
+@end
+
+@implementation JDOBusLIstViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self loadDataFromNetwork];
+    tablelist.delegate = self;
+    tablelist.dataSource = self;
+}
+
+- (void)loadDataFromNetwork{
+    buslines = [[NSMutableArray alloc] init];
+    NSDictionary *params = @{@"channelid" : @"19", @"pageSize" : @"1000"};
+    [[JDOHttpClient sharedClient] getJSONByServiceName:NEWS_SERVICE modelClass:@"JDONewsModel" params:params success:^(NSArray *dataList) {
+        if(dataList == nil){
+            
+        }else if(dataList.count >0){
+            [buslines addObjectsFromArray:dataList];
+            [tablelist reloadData];
+        }
+    } failure:^(NSString *errorStr) {
+        
+    }];
+}
+
+- (void)setupNavigationView{
+    [self.navigationView addBackButtonWithTarget:self action:@selector(onBackBtnClick)];
+    [self.navigationView setTitle:@"公交班次"];
+}
+
+- (void) onBackBtnClick{
+    JDOCenterViewController *centerViewController = (JDOCenterViewController *)self.navigationController;
+    [centerViewController popToViewController:[centerViewController.viewControllers objectAtIndex:0] animated:true];
+}
+
+#pragma mark UITableViewDelegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 35.;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+	return 0.;
+}
+
+#pragma mark -
+#pragma mark UITableViewDataSource
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = nil;
+    cell = [[UITableViewCell alloc] init];
+    if (buslines.count >= indexPath.row) {
+        cell.textLabel.text = [[buslines objectAtIndex:indexPath.row] title];
+    }
+	return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	return buslines.count;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+	return 1;
+}
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
