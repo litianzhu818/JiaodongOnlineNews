eeeff40949865a295d3a62ae3397f7e6e76980f6
陈鹏 <chenpeng@jiaodongdeMacBook-Pro.local>
2013-07-25 08:18:52 +0800

    collectdb

---
+++ JiaodongOnlineNews/Classes/model/JDOCollectDB.h
@@ -0,0 +1,13 @@
+//
+//  JDOCollectDB.h
+//  JiaodongOnlineNews
+//
+//  Created by 陈鹏 on 13-7-25.
+//  Copyright (c) 2013年 胶东在线. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JDOCollectDB : NSObject
+
+@end
---
+++ JiaodongOnlineNews/Classes/model/JDOCollectDB.m
@@ -0,0 +1,13 @@
+//
+//  JDOCollectDB.m
+//  JiaodongOnlineNews
+//
+//  Created by 陈鹏 on 13-7-25.
+//  Copyright (c) 2013年 胶东在线. All rights reserved.
+//
+
+#import "JDOCollectDB.h"
+
+@implementation JDOCollectDB
+
+@end
