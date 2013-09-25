779ceb96048f1cb7bea96737059f83504814b5ab
zhang yi <intotherainzy@gmail.com>
2013-09-25 18:02:14 +0800

    1.修改iOS7下输入评论超过字数限制后继续输入拼音会闪退的bug 2.修改iOS7输入框高度无法根据内容高度自动扩展的bug

--- JiaodongOnlineNews/Classes/Controllers/JDOShareViewController.m
+++ JiaodongOnlineNews/Classes/Controllers/JDOShareViewController.m
@@ -306,7 +306,12 @@
             self.remainWordLabel.text = [NSString stringWithFormat:@"还可以输入%d字",remaind];
         } else {
             self.remainWordLabel.text = [NSString stringWithFormat:@"还可以输入%d字",0];
-            _textView2.text = [_textView2.text substringWithRange:NSMakeRange(0, Review_Content_MaxLength - [_titleLabel.text length])];
+            if (NSFoundationVersionNumber > NSFoundationVersionNumber_iOS_6_1) {
+                // iOS7 文本输入到最大值的时候再输入汉字状态的拼音会闪退
+            }else{
+                _textView2.text = [_textView2.text substringWithRange:NSMakeRange(0, Review_Content_MaxLength - [_titleLabel.text length])];
+            }
+            
         }
     }
 }
