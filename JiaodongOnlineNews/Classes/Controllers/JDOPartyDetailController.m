60ac9da1561353673d2c6011fd72c9281b13bebd
liubin <liubinjaywln@163.com>
2013-09-26 14:30:29 +0800

    commit

--- JiaodongOnlineNews/Classes/Controllers/JDOPartyDetailController.m
+++ JiaodongOnlineNews/Classes/Controllers/JDOPartyDetailController.m
@@ -16,7 +16,6 @@
 
 @interface JDOPartyDetailController ()
 
-@property (strong, nonatomic) WebViewJavascriptBridge *bridge;
 @property (strong, nonatomic) UITapGestureRecognizer *closeReviewGesture;
 @property (strong, nonatomic) UIView *blackMask;
 
@@ -59,7 +58,7 @@ NSArray *imageUrls;
 
 - (void) buildWebViewJavascriptBridge{
     [super buildWebViewJavascriptBridge];
-    [_bridge registerHandler:@"joinInParty" handler:^(id data, WVJBResponseCallback responseCallback) {
+    [super.bridge registerHandler:@"joinInParty" handler:^(id data, WVJBResponseCallback responseCallback) {
         NSString *joinBtnStr = [(NSDictionary *)data valueForKey:@"joinBtnStr"];
         if ([joinBtnStr isEqualToString:@"我要报名"]) {
             [[JDOJsonClient sharedClient] getPath:PARTY_JOIN_SERVICE parameters:@{@"aid":self.partyModel.id} success:^(AFHTTPRequestOperation *operation, id responseObject) {
